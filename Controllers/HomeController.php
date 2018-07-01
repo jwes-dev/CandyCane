0298d1ead4a1c2027afe2e546ef8208c3be98cd0
John Wesley M <jwes.devstudio@gmail.com>
2018-07-01 16:07:29 +0530

    Update HomeController.php

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -20,27 +20,5 @@ class HomeController extends Controller
     {
         $this->View();
     }
-
-    public function GotoIndex()
-    {
-        Response::RedirectTo("Home", "Index");
-    }
-
-    public function GetTableData()
-    {
-        $this->db = NewSQLConnection();
-        $data = $this->db->query("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Check'");
-        $obj = new chk();
-        $reflect = new ReflectionClass($obj);
-        $props = $reflect->getProperties(ReflectionProperty::IS_PUBLIC);
-        $sel = "";
-        while($seg = $data->fetch_assoc())
-        {
-            if($seg["DATA_TYPE"] == "date")
-                $sel += "";
-            var_dump($seg);
-            echo "<br /><br />";
-        }
-    }
 }
 ?>
