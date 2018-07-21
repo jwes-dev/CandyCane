8ae8aab4655e9d0bb830f069a5f47e5ca1cfda8c
John Wesley M <jwes.devstudio@gmail.com>
2018-07-21 22:40:08 +0530

    Add files via upload

--- StartUp.php
+++ StartUp.php
@@ -1,11 +1,10 @@
 <?php
-
 spl_autoload_register(function ($class_name) {
-    if(file_exists(Application::$AppData->ServerPath."/Models/$class_name.php"))
-        require_once Application::$AppData->ServerPath."/Models/$class_name.php";
+    if (file_exists(Application::$AppConfig->ServerPath . "/Models/$class_name.php"))
+        require_once Application::$AppConfig->ServerPath . "/Models/$class_name.php";
 
-    // else if(file_exists(Application::$AppData->ServerPath."/Helpers/$class_name.php"))
-    //     require_once Application::$AppData->ServerPath."/Helpers/$class_name.php";
+    // else if(file_exists(Application::$AppConfig->ServerPath."/Helpers/$class_name.php"))
+    //     require_once Application::$AppConfig->ServerPath."/Helpers/$class_name.php";
 });
 date_default_timezone_set('UTC');
 
@@ -26,69 +25,69 @@ require_once "App_Start/RouteConfig.php";
 // require_once "Controllers/HomeController.php";
 
 
-$reqpath = strtok("/".trim(substr($_SERVER["REQUEST_URI"], strlen(Application::$AppData->AppPath)), "/"), "?");
+$reqpath = strtok("/" . trim(substr($_SERVER["REQUEST_URI"], strlen(Application::$AppConfig->AppPath)), "/"), "?");
 Request::$Url = $_SERVER["REQUEST_URI"];
-Context::$ViewName = Application::$AppData->ServerPath;
-foreach(ROUTES as $name =>$path)
-{
+Context::$ViewName = Application::$AppConfig->ServerPath;
+foreach (ROUTES as $name => $path) {
     $cname = explode("/", substr($reqpath, strlen($path[0])));
-    $find = Application::$AppData->ServerPath.$path[1]."/Controllers/$cname[0]Controller.php";
-    if(file_exists($find))
-    {
+    $find = Application::$AppConfig->ServerPath . $path[1] . "/Controllers/$cname[0]Controller.php";
+    if (file_exists($find)) {
         // complete path given
         require_once $find;
         $controller = "$cname[0]Controller";
+        if (!class_exists($controller))
+            Response::SetStatusCodeResult(404, "Not Found");
+        ResolveClassFilters($controller);
         Context::$WorkingDir = $path[1];
         Context::$Controller = $cname[0];
-        Context::$ViewName .= $path[1]."Views";
-        if(isset($cname[1]))
-        {
+        Context::$ViewName .= $path[1] . "Views";
+        if (isset($cname[1])) {
             $method = $cname[1];
-            if(method_exists($controller, $method))
-            {
+            if (method_exists($controller, $method)) {
                 Context::$Method = $method;
                 // var_dump($Context);
-                if(isset($cname[2]))
-                    $_GET[$path[4]] = $cname[2];
+                if (isset($cname[2]))
+                    $_REQUEST[$path[4]] = $cname[2];
                 $cont = new $controller();
+                ResolveMethodFilters($controller, $method);
                 $cont->$method();
                 exit;
             }
-        }
-        else
-        {
+        } else {
             $method = $path[3];
-            if(method_exists($controller, $method))
-            {
+            if (method_exists($controller, $method)) {
                 // ony the controller name is given
                 Context::$Method = $method;
                 // var_dump($Context);
-                if(isset($cname[2]))
-                    $_GET[$path[4]] = $cname[2];
+                if (isset($cname[2]))
+                    $_REQUEST[$path[4]] = $cname[2];
                 $cont = new $controller();
+                ResolveMethodFilters($controller, $method);
                 $cont->$method();
                 exit;
             }
         }
-        
+
     }
 }
 $ROUTES = array_reverse(ROUTES);
-foreach($ROUTES as $n=>$path)
-{
-    if($reqpath."/" == $path[0] || $reqpath == $path[0])
-    {
+foreach ($ROUTES as $n => $path) {
+    if ($reqpath . "/" == $path[0] || $reqpath == $path[0]) {
         //only the area name is given
         $controller = "$path[2]Controller";
-        require_once Application::$AppData->ServerPath.$path[1]."Controllers/$controller.php";
+        require_once Application::$AppConfig->ServerPath . $path[1] . "Controllers/$controller.php";
+        if (!class_exists($controller))
+            Response::SetStatusCodeResult(404, "Not Found");
+        ResolveClassFilters($controller);
+
         $method = $path[3];
-        if(method_exists($controller, $method))
-        {
+        if (method_exists($controller, $method)) {
             Context::$WorkingDir = $path[1];
             Context::$Controller = $path[2];
             Context::$Method = $method;
-            Context::$ViewName .= $path[1]."Views";
+            Context::$ViewName .= $path[1] . "Views";
             $cont = new $controller();
+            ResolveMethodFilters($controller, $method);
             $cont->$method();
             exit;
         }
