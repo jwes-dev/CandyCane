db8e92b01772c3dde0087650ea8c017ec76ddaea
John Wesley M <jwes.devstudio@gmail.com>
2018-07-21 22:39:52 +0530

    Add files via upload

---
+++ Reference/Filters.php
@@ -0,0 +1,66 @@
+<?php
+
+function ResolveClassFilters($class)
+{
+    $ref = new ReflectionClass($class);
+    $comment = $ref->getDocComment();
+    $attrs = explode("\n", trim(trim($comment, "/**")));
+    if(trim($attrs[0]) != "FILTERS")
+        return;
+    foreach($attrs as $attr)
+    {
+        if(trim($attr) != "FILTERS")
+        {
+            $f = explode(" ", trim($attr), 2);
+            if(!file_exists(R::File("~/Filters/".$f[0]."Filter")))
+                return false;
+            require_once R::File("~/Filters/".$f[0]."Filter");
+            if(!class_exists($f[0]."Filter"))
+                return false;
+            $fil = $f[0]."Filter";
+            if(isset($f[1]))
+                if(strlen($f[1]) > 0)
+                    $filter = new $fil(json_decode($f[1]));
+            else
+                $filter = new $fil();
+            return true;
+        }
+    }
+}
+
+function ResolveMethodFilters($class, $method)
+{
+    $ref = new ReflectionClass($class);
+    $comment = $ref->getMethod($method)->getDocComment();
+    $attrs = explode("\n", trim(trim($comment, "/**")));
+    if(trim($attrs[0]) != "FILTERS")
+        return;
+    foreach($attrs as $attr)
+    {
+        if(trim($attr) != "FILTERS")
+        {
+            $f = explode(" ", trim($attr), 2);
+            if(!file_exists(R::File("~/Filters/".$f[0]."Filter.php")))
+                return false;
+            require_once R::File("~/Filters/".$f[0]."Filter.php");
+            if(!class_exists($f[0]."Filter"))
+                return false;
+            $fil = $f[0]."Filter";
+            if(isset($f[1]))
+                if(strlen($f[1]) > 0)
+                    $filter = new $fil(json_decode($f[1]));
+            else
+                $filter = new $fil();
+            return true;
+        }
+    }
+}
+
+class Filter
+{
+    public function __construct($args)
+    {
+        $this->OnBeforeExecute($args);
+    }
+}
+?>
