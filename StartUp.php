ebcb3c680140f72495cda0018c13dae8e939f492
John Wesley M <johnwesley.m@outlook.com>
2019-02-17 21:50:14 +0530

    Add files via upload

--- StartUp.php
+++ StartUp.php
@@ -1,10 +1,10 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    if (file_exists(Application::$AppConfig->ServerPath . "/Models/$class_name.php"))
-        require_once Application::$AppConfig->ServerPath . "/Models/$class_name.php";
+    if (file_exists(Application::$AppData->ServerPath . "/Models/$class_name.php"))
+        require_once Application::$AppData->ServerPath . "/Models/$class_name.php";
 
-    else if(file_exists(Application::$AppConfig->ServerPath."/Helpers/$class_name.php"))
-        require_once Application::$AppConfig->ServerPath."/Helpers/$class_name.php";
+    else if(file_exists(Application::$AppData->ServerPath."/Helpers/$class_name.php"))
+        require_once Application::$AppData->ServerPath."/Helpers/$class_name.php";
 });
 date_default_timezone_set('UTC');
 
@@ -21,13 +21,12 @@ require_once "App_Start/BundlesConfig.php";
 require_once "App_Start/RouteConfig.php";
 
 
-
-$reqpath = strtok("/" . trim(substr($_SERVER["REQUEST_URI"], strlen(Application::$AppConfig->AppPath)), "/"), "?");
+$reqpath = strtok("/" . trim(substr($_SERVER["REQUEST_URI"], strlen(Application::$AppData->AppPath)), "/"), "?");
 Request::$Url = $_SERVER["REQUEST_URI"];
-Context::$ViewName = Application::$AppConfig->ServerPath;
+Context::$ViewName = Application::$AppData->ServerPath;
 foreach (RouteConfig::$Routes as $name => $path) {
     $cname = explode("/", substr($reqpath, strlen($path[0])));
-    $find = Application::$AppConfig->ServerPath . $path[1] . "Controllers/$cname[0]Controller.php";
+    $find = Application::$AppData->ServerPath . $path[1] . "Controllers/$cname[0]Controller.php";
     if (file_exists($find)) {
         // complete path given
         require_once $find;
@@ -46,7 +45,8 @@ foreach (RouteConfig::$Routes as $name => $path) {
                     $_REQUEST[$path[4]] = $cname[2];
                 $cont = new $controller();
                 ResolveMethodFilters($controller, $method);
-                $cont->__Initialize__();
+                if(method_exists($controller, "__Initialize__"))                
+                    $cont->__Initialize__();
                 $cont->$method();
                 exit;
             }
@@ -60,7 +60,8 @@ foreach (RouteConfig::$Routes as $name => $path) {
                     $_REQUEST[$path[4]] = $cname[2];
                 $cont = new $controller();
                 ResolveMethodFilters($controller, $method);
-                $cont->__Initialize__();
+                if(method_exists($controller, "__Initialize__"))                
+                    $cont->__Initialize__();
                 $cont->$method();
                 exit;
             }
@@ -73,7 +74,7 @@ foreach ($ROUTES as $n => $path) {
     if ($reqpath . "/" == $path[0] || $reqpath == $path[0]) {
         //only the area name is given
         $controller = "$path[2]Controller";
-        require_once Application::$AppConfig->ServerPath . $path[1] . "Controllers/$controller.php";
+        require_once Application::$AppData->ServerPath . $path[1] . "Controllers/$controller.php";
         if (!class_exists($controller))
             Response::SetStatusCodeResult(404, "Not Found");
         ResolveClassFilters($controller);
