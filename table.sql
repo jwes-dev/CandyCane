5134755f2de2f859adec6204d5a5504f9f4ccd9f
John Wesley M <johnwesley.m@outlook.com>
2019-02-25 22:16:53 +0530

    Delete table.sql

--- table.sql
+++
@@ -1,9 +0,0 @@
-create table snowkeld_admin_Users(
-    Email varchar(255) PRIMARY KEY,
-    Secret varchar(255) NOT NULL
-);
-
-create table snowkeld_admin_Roles(
-    Email varchar(255) PRIMARY KEY,
-    Roles varchar(255)
-);
