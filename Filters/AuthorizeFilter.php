dff516e05a4e5e11ee2650c1259c9c76aa83eb65
John Wesley M <johnwesley.m@outlook.com>
2019-03-06 16:41:15 +0530

    Add files via upload

--- Filters/AuthorizeFilter.php
+++ Filters/AuthorizeFilter.php
@@ -1,13 +1,26 @@
 <?php
 class AuthorizeFilter extends Filter
 {
-    public function OnBeforeExecute($roles)
-    {        
+    public function OnBeforeExecute($args)
+    {
         if(!isset($_SESSION["Email"]))
         {
-            if(FilterHelper::ResolveMethodFilters($this->Context) !== -1)
-                Response::Redirect("~/Account/Login".(strlen(Request::$Url) > 0 ? "?url=".Request::$Url : ""));
+            echo "email failed";
+            die();
+            Response::Redirect("~/Account/Login".(strlen(Request::$Url) > 0 ? "?url=".Request::$Url : ""));
         }
+        else if(FilterHelper::ResolveMethodFilters($this->Context) === -1)
+        {
+            return true;
+        }
+        if($args != null)
+        {
+            if(count(array_intersect($_SESSION["Roles"], $args->InRoles)) < 1)
+            {
+                Response::Redirect("~/Account/Login".(strlen(Request::$Url) > 0 ? "?url=".Request::$Url : ""));
+            }
+        }
+        return true;
     }
 }
 ?>
